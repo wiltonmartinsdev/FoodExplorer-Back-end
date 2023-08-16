8d87bea63b4b4016a6ce531734fd2929850a1ce3
Wilton Lira Martins <wiltonliramartins@gmail.com>
2023-08-16 00:20:14 -0300

    <Refactor>: Project refactoring

--- src/controllers/UsersController.js
+++ src/controllers/UsersController.js
@@ -6,7 +6,7 @@ const { hash } = pkg;
 
 class UsersController {
 	async create(request, response) {
-		let { name, email, password } = request.body;
+		let { name, email, password, adminUser } = request.body;
 
 		try {
 			await knex.transaction(async (trx) => {
@@ -23,7 +23,7 @@ class UsersController {
 				const hashedPassword = await hash(password, 8);
 				password = hashedPassword;
 
-				await trx("users").insert({ name, email, password });
+				await trx("users").insert({ name, email, password, adminUser });
 
 				return response
 					.status(201)
